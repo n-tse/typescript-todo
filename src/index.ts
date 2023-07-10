d777e8347cbe7d34ca0331661ee65994e5ddbb3c
Nick Tse <nicholashtse@gmail.com>
2023-07-10 12:24:38 -0400

    add task type and task creation functionality

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,42 @@
-import { v4 as uuidV4 } from 'uuid';
+import { v4 as uuidV4 } from "uuid";
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
 const list = document.querySelector<HTMLUListElement>("#list");
-const form = document.getElementById("#new-task-form") as HTMLFormElement | null;
+const form = document.getElementById(
+  "new-task-form"
+) as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
 
-form?.addEventListener("submit", e => {
+form?.addEventListener("submit", (e) => {
   e.preventDefault();
   // ? is for optional chaining, since value of 'input' could possibly be null if id doesn't exist
   // basically ensures that 'input' will exist for any following lines
   if (input?.value == "" || input?.value == null) return;
 
-  const task = {
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date(),
-  }
-})
+  };
+
+  addListItem(newTask);
+  input.value = "";
+});
+
+function addListItem(task: Task) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
