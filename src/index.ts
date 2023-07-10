c9eeca765e3d9373c6481d6cf5a5af41dc6ba2c4
Nick Tse <nicholashtse@gmail.com>
2023-07-10 13:51:31 -0400

    add button to delete task; update spacing around list items

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,7 @@ type Task = {
 };
 
 const list = document.querySelector<HTMLUListElement>("#list");
-const form = document.getElementById(
-  "new-task-form"
-) as HTMLFormElement | null;
+const form = document.getElementById("new-task-form") as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
 const tasks: Task[] = loadTasks();
 tasks.forEach(addListItem);
@@ -37,15 +35,25 @@ form?.addEventListener("submit", (e) => {
 
 function addListItem(task: Task) {
   const item = document.createElement("li");
+  item.dataset.id = task.id;
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.className = "delete-task-button";
+
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
   checkbox.addEventListener("change", () => {
     task.completed = checkbox.checked;
     saveTasks();
-  })
-  checkbox.type = "checkbox";
-  checkbox.checked = task.completed;
-  label.append(checkbox, task.title);
+  });
+
+  deleteButton.addEventListener("click", () => {
+    deleteTask(task);
+  });
+
+  label.append(checkbox, task.title, deleteButton);
   item.append(label);
   list?.append(item);
 }
@@ -58,4 +66,16 @@ function loadTasks(): Task[] {
   const taskJSON = localStorage.getItem("TASKS");
   if (taskJSON === null) return [];
   return JSON.parse(taskJSON);
+}
+
+function deleteTask(task: Task) {
+  const index = tasks.findIndex((t) => t.id === task.id);
+  if (index !== -1) {
+    tasks.splice(index, 1);
+    saveTasks();
+    const listItem = document.querySelector(`li[data-id="${task.id}"]`);
+    if (listItem) {
+      listItem.remove();
+    }
+  }
 }
